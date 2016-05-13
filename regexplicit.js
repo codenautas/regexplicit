b656fa9a39e3fdcd1912cfc24880332ae23148af
estefi-capece <eycapece@gmail.com>
2016-05-13 15:39:50 -0300

    regexplicit.variables

--- regexplicit.js
+++ regexplicit.js
@@ -1,11 +1,33 @@
 "use strict";
 /*jshint eqnull:true */
-/*jshint globalstrict:true */
 /*jshint node:true */
-/*eslint-disable no-console */
 
+(function codenautasModuleDefinition(root, name, factory) {
+    /* global define */
+    /* istanbul ignore next */
+    if(typeof root.globalModuleName !== 'string'){
+        root.globalModuleName = name;
+    }
+    /* istanbul ignore next */
+    if(typeof exports === 'object' && typeof module === 'object'){
+        module.exports = factory();
+    }else if(typeof define === 'function' && define.amd){
+        define(factory);
+    }else if(typeof exports === 'object'){
+        exports[root.globalModuleName] = factory();
+    }else{
+        root[root.globalModuleName] = factory();
+    }
+    root.globalModuleName = null;
+})(/*jshint -W040 */this, 'Regexplicit', function() {
+/*jshint +W040 */
+
+/*jshint -W004 */
 var Regexplicit = {};
+/*jshint +W004 */
 
 Regexplicit.variables = /\b[A-Za-z_]\w*(?!\s*\()\b/g;
 
-module.exports = Regexplicit;
+return Regexplicit;
+
+});
