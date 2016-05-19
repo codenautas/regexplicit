54f6061efd484fa7f25efaa472ae9a75dbebd8a2
estefi-capece <eycapece@gmail.com>
2016-05-19 12:30:22 -0300

    Detectamos el operador igual

--- regexplicit.js
+++ regexplicit.js
@@ -26,7 +26,39 @@
 var Regexplicit = {};
 /*jshint +W004 */
 
-Regexplicit.variables = /\b[A-Za-z_]\w*(?!\s*\()\b/g;
+/// EN GENERAL
+/// Salvo que se indique lo contrario todos los regexp tienen la forma izquierda, detectado, derecha
+/// cuando dice ISSUE: no reconoce string significa que puede identificar cosas dentro de un string como si fueran otra cosa.
+
+/// DESCRIPCIÓN: identificar nombre de variables en una expresión distingiuéndolas de nombres de funciones
+/// ISSUES: no reconoce strings, ni comentarios
+/// USO: directo
+Regexplicit.variables = /(\b)([A-Za-z_]\w*)(?!\s*\()(\b)/g;
+/*
+Regexplicit.variables.replacer = function replacer(string, replacer){
+    return string.replace(Regexplicit.variables, function(match){
+        return replacer(match);
+    });
+};
+*/
+
+/// DESCRIPCIÓN: identificar el operador = para poder reemplazarlo por ==, logra distinguir el = de un != >= == etc. 
+/// ISSUES: no reconoce strings, ni comentarios
+/// USO: detecta izquierda, operador(=), derecha
+Regexplicit.operadorIgual = /(^|[^!><=])(=)($|[^=])/g;
+/*
+Regexplicit.operadorIgual.replacer = function replacer(string, detectedReplacer){
+    return string.replace(Regexplicit.operadorIgual, function(match, left, detected, right){
+        return left+detectedReplacer(detected)+right;
+    });
+}
+*/
+
+RegExp.prototype.replacer = function replacer(string, detectedReplacer){
+    return string.replace(this, function(match, left, detected, right){
+        return left+(typeof detectedReplacer === 'string'?detectedReplacer:detectedReplacer(detected))+right;
+    });
+}
 
 return Regexplicit;
 
--- test/test.js
+++ test/test.js
@@ -6,15 +6,36 @@ var rgl = require('../regexplicit.js');
 
 describe("regexps", function(){
     [
-        {expression:"uno = 15", expect: "<<uno>> = 15"},
+        {expression:"uno = 15"          , expect: "<<uno>> = 15"},
         {expression:"(uno + dos)*b = 15", expect: "(<<uno>> + <<dos>>)*<<b>> = 15"},
-        {expression:"f(uno)", expect: "f(<<uno>>)"},
-        {expression:"f (uno)", expect: "f (<<uno>>)"},
+        {expression:"f(uno)"            , expect: "f(<<uno>>)"},
+        {expression:"f (uno)"           , expect: "f (<<uno>>)"},
     ].forEach(function(fixture){
         it("matchs variables in expression, fixture: "+JSON.stringify(fixture), function(){
-            var variables = rgl.variables;
             // eval(assert(fixture.expression.replace(variables, function(match){ return "<<"+match+">>"; }) === fixture.expect));
-            expect(fixture.expression.replace(variables, function(match){ return "<<"+match+">>"; })).to.eql(fixture.expect);
+            expect(fixture.expression.replace(rgl.variables, function(match){ return "<<"+match+">>"; })).to.eql(fixture.expect);
+            expect(fixture.expression.replace(rgl.variables, function(match, left, center, right){ return left+"<<"+center+">>"+right; })).to.eql(fixture.expect);
+            expect(rgl.variables.replacer(fixture.expression, function(variable){ return "<<"+variable+">>"; })).to.eql(fixture.expect);
+        });
+    });
+    [
+        {expression:"uno = 15"           , expect: "uno [=] 15"},
+        {expression:"uno=15"             , expect: "uno[=]15"},
+        {expression:"uno=(15)"           , expect: "uno[=](15)"},
+        {expression:"(uno)=15"           , expect: "(uno)[=]15"},
+        {expression:"(uno)={a:15}"       , expect: "(uno)[=]{a:15}"},
+        {expression:"uno==15"            , expect: "uno==15"},
+        {expression:"uno!=15"            , expect: "uno!=15"},
+        {expression:"uno!==15"           , expect: "uno!==15"},
+        {expression:"uno='15'"           , expect: "uno[=]'15'"},
+        {expression:"uno===15"           , expect: "uno===15"},
+        {expression:"uno>=15"            , expect: "uno>=15"},
+        {expression:"a=b && b=c"         , expect: "a[=]b && b[=]c"},
+    ].forEach(function(fixture){
+        it("matchs = in expression, fixture: "+JSON.stringify(fixture), function(){
+            // eval(assert(fixture.expression.replace(operadorIgual, function(match){ return "["+match+"]"; }) === fixture.expect));
+            expect(rgl.operadorIgual.replacer(fixture.expression,function(match){ return "["+match+"]"; })).to.eql(fixture.expect);
+            expect(rgl.operadorIgual.replacer(fixture.expression,"[=]")).to.eql(fixture.expect);
         });
     });
 });
